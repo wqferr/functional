4a18d6aeee000b82811284a4194527204e22f90d
William Ferreira <wqferr@gmail.com>
2022-08-13 17:31:09 -0300

    Update tests to new lambda syntax

--- test/test-newlambda.lua
+++ test/test-newlambda.lua
@@ -1,11 +1,11 @@
 require "reqpath"
 local f = require "functional"
 
-local l = f.lambda2 "(x) => (y)   => x+y"
+local l = f.lambda "(x) => (y)   => x+y"
 print(l(1)(3))
 print(l.body)
 print(tostring(l))
-print(f.lambda2(("()=>"):rep(5) .. "'lol'").body)
+print(f.lambda(("()=>"):rep(5) .. "'lol'").body)
 
 assert(not pcall(f.lambda2, "(x) (what) => 3*x"))
 assert(not pcall(f.lambda2, ("()=>"):rep(20) .. "'lol'"))
